f2c2e3680f9fdddb02d002a1b0abc184b816a34e
Suchitra Naik <susanaik@gmail.com>
2018-05-01 16:33:40 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -8,11 +8,10 @@ class Shoe
   def initialize(brand)
       @brand = brand 
       BRANDS << brand 
+      BRANDS.uniq 
   end 
   
-  def self.uniqBrands 
-    BRANDS.uniq 
-  end 
+ 
   
   def cobble
     self.condition = "new"
